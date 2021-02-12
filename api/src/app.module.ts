fbabcf1c052c7f0c780a768e715a93467e22ba90
unknown <neonkamuy@gmail.com>
2021-02-12 07:05:08 +0400

    Added Serve-Static

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,11 +1,17 @@
 import { Module } from '@nestjs/common';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 import { UserController } from './controllers/users';
 import UserModel from './models/users';
 import UserService from './services';
 import TYPES from './types';
 
 @Module({
-  imports: [],
+  imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '../../../front/build'),
+    }),
+  ],
   controllers: [UserController],
   providers: [UserService, { provide: TYPES.Models.User, useValue: UserModel }],
 })
