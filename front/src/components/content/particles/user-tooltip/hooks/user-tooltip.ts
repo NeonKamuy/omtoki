040d40f34614f94976f1b75057003655727581b8
unknown <neonkamuy@gmail.com>
2021-02-13 05:29:47 +0400

    Fixed tooltip disappearance

--- front/src/components/content/particles/user-tooltip/hooks/user-tooltip.ts
+++ front/src/components/content/particles/user-tooltip/hooks/user-tooltip.ts
@@ -24,7 +24,7 @@ export function useUserTooltip() {
         };
 
         const eventOffListener = () => {
-            // hide();
+            hide();
         };
 
         document.addEventListener(__SETTINGS__.TOOLTIP.eventOn, eventOnListener);
