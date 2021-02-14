efd711262c08103f004fff1c6b8ce3d9766963ae
unknown <neonkamuy@gmail.com>
2021-02-15 00:27:54 +0400

    Added <head> with react-helmet

--- front/src/index.tsx
+++ front/src/index.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './views/App';
 import 'bootstrap/dist/css/bootstrap.css';
 import './index.scss';
+import App from './views/App';
 
 ReactDOM.render(
   <React.StrictMode>
--- front/src/views/App.tsx
+++ front/src/views/App.tsx
@@ -1,9 +1,15 @@
 import React from "react";
 import { ParticlesView } from "./particles";
+import {Helmet} from "react-helmet";
 
 const App = () => {
     return (
-        <div style={{height: "100%"}}>
+        <div style={{ height: "100%" }}>
+            <Helmet>
+                <link rel="icon" href="https://icon-library.com/images/progress-icon-gif/progress-icon-gif-21.jpg" type="image/gif" ></link>
+                <title>Omtoki</title>
+            </Helmet>
+
             <ParticlesView />
         </div>
     );
