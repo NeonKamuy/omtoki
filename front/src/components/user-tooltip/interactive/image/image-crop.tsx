ea06c4da9f2e2375555c833a0f72ee7a994fc41d
hfwalter73 <hfwalter73@gmail.com>
2021-02-17 01:23:35 +0400

    Replaced image crop library

--- front/src/components/user-tooltip/interactive/image/image-crop.tsx
+++ front/src/components/user-tooltip/interactive/image/image-crop.tsx
@@ -1,19 +1,30 @@
-import React, { useCallback, useState } from "react";
+import React, { useCallback, useRef, useState } from "react";
+import Cropper from "react-cropper";
+import "cropperjs/dist/cropper.css";
 import "../style.scss";
-import ReactCrop from "react-image-crop";
-import "react-image-crop/lib/ReactCrop.scss";
 
 export const ImageCrop: React.FC<{ image: string }> = (props) => {
     const { image } = props;
-    const [crop, setCrop] = useState<ReactCrop.Crop>({ aspect: 16 / 9 });
+
+    const cropperRef = useRef<HTMLImageElement>(null);
+    const onCrop = () => {
+        const imageElement: any = cropperRef?.current;
+        const cropper: any = imageElement?.cropper;
+        console.log(cropper.getCroppedCanvas().toDataURL());
+    };
 
     return (
-        <div className={"interactive-user-tooltip-img-crop-container"}>
-            <ReactCrop
-                src={image}
-                crop={crop}
-                onChange={(newCrop) => setCrop(newCrop)}
-            />
-        </div>
+        <Cropper
+            src={image}
+            className="interactive-user-tooltip-img-crop-container"
+            initialAspectRatio={1}
+            aspectRatio={1}
+            guides={true}
+            movable={false}
+            scalable={false}
+            zoomable={false}
+            crop={onCrop}
+            ref={cropperRef}
+        />
     );
 };
