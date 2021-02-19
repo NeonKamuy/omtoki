1bdeba837cdf74ce3fb19c58abedb10269ac4a0d
hfwalter73 <hfwalter73@gmail.com>
2021-02-19 18:12:56 +0400

    Fixed particle collision problems

--- front/src/views/particles/content/index.tsx
+++ front/src/views/particles/content/index.tsx
@@ -4,7 +4,16 @@ import { IIndexedUser } from "../../../shared/interfaces/user";
 import { ParticleContainer } from "../../../components/canvas";
 
 export const Content: React.FC<{}> = () => {
-    const elements: IIndexedUser[] = useUsers();
+    const elements: IIndexedUser[] = /*useUsers()*/ new Array(500)
+        .fill(0)
+        .map((e) => ({
+            description: "asdas",
+            id: Math.random() * 1000000 + "",
+            name: "asldkjals",
+            picture: "alskdj",
+            skills: "alskdj",
+            tg: "laskdj",
+        }));
 
     return (
         <div>
