6c1ce433cde2d2782c96f16c86c5d429f2037298
hfwalter73 <hfwalter73@gmail.com>
2021-02-19 19:53:37 +0400

    Removed dummy elements

--- front/src/views/particles/content/index.tsx
+++ front/src/views/particles/content/index.tsx
@@ -4,16 +4,7 @@ import { IIndexedUser } from "../../../shared/interfaces/user";
 import { ParticleContainer } from "../../../components/canvas";
 
 export const Content: React.FC<{}> = () => {
-    const elements: IIndexedUser[] = /*useUsers()*/ new Array(500)
-        .fill(0)
-        .map((e) => ({
-            description: "asdas",
-            id: Math.random() * 1000000 + "",
-            name: "asldkjals",
-            picture: "alskdj",
-            skills: "alskdj",
-            tg: "laskdj",
-        }));
+    const elements: IIndexedUser[] = useUsers();
 
     return (
         <div>
