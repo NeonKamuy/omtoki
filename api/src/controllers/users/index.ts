bc85fd84dbb86bdf8bfe5446ef20736b481c073b
unknown <neonkamuy@gmail.com>
2021-03-27 19:04:59 +0400

    Fixed images

--- api/src/controllers/users/index.ts
+++ api/src/controllers/users/index.ts
@@ -1,40 +1,51 @@
-import { Controller, Get, Inject, Post, Res } from '@nestjs/common';
-import { IIndexedUser, IIndexedUserMeta, IUserBase } from 'shared/interfaces/user';
-import UserService from 'src/services';
-import TYPES from 'src/types';
-import { wValidatedArg } from '../decorators/validation';
-import { UserBaseSchema } from './helper-schemas';
-import { AGETPictureByUserIdSchema, IAGETAllUsers, IAGETPictureByUserId } from './validators';
-import {Response} from "express";
-import fs from "fs";
+import { Controller, Get, Header, Inject, Post, Res } from "@nestjs/common";
+import { IUserBase } from "shared/interfaces/user";
+import UserService from "src/services";
+import TYPES from "src/types";
+import { wValidatedArg } from "../decorators/validation";
+import { UserBaseSchema } from "./helper-schemas";
+import {
+    AGETPictureByUserIdSchema,
+    IAGETAllUsers,
+    IAGETPictureByUserId,
+} from "./validators";
+import { Response } from "express";
+import * as tmp from "tmp";
+import * as fs from "fs";
+
 
 @Controller("/api/users")
 export class UserController {
-  constructor(
-    private readonly _UserService: UserService
-  ) {}
+    constructor(private readonly _UserService: UserService) {}
 
-  @Get("/")
-  public getAll(): Promise<IAGETAllUsers> {
-    return this._UserService.getAll();
-  }
+    @Get("/")
+    public getAll(): Promise<IAGETAllUsers> {
+        return this._UserService.getAll();
+    }
 
- 
-  @Get("/picture/:userId")
-  public async getPictureByUserId(
-    @wValidatedArg(AGETPictureByUserIdSchema) args: IAGETPictureByUserId,
-    @Res() res: Response
-  ) {
-    const picture = await this._UserService.getPictureByUserId(args);
-    const buffer = Buffer.from(picture, "base64");
-    const stream = fs.createReadStream(buffer);
-    stream.pipe(res);
-  }
+    @Get("/picture/:userId")
+    @Header("Content-Type", "image/*")
+    public async getPictureByUserId(
+        @wValidatedArg(AGETPictureByUserIdSchema) args: IAGETPictureByUserId,
+        @Res() res: Response
+    ) {
+        const picture = await this._UserService.getPictureByUserId(args);
+        const buffer = Buffer.from(picture.replace(/^data:image\/[a-z]+;base64,/, ""), "base64");
+        tmp.dir({}, (e, tempDir)=>{
+            const filename = `${tempDir}/${Date.now() + args.userId}`;
+            fs.writeFile(filename, buffer, ()=>{
+                res.sendFile(filename);
+                res.once("finish", ()=>{
+                    fs.unlink(filename, ()=>{});
+                })
+            });
+        });
+    }
 
-  @Post("/")
-  public addUser(
-    @wValidatedArg(UserBaseSchema) args: IUserBase
-  ): Promise<IAGETAllUsers> {
-    return this._UserService.addUser(args);
-  }
+    @Post("/")
+    public addUser(
+        @wValidatedArg(UserBaseSchema) args: IUserBase
+    ): Promise<IAGETAllUsers> {
+        return this._UserService.addUser(args);
+    }
 }
