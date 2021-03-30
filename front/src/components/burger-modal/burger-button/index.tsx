ca669962f25185a2bddf5b1023d7f5619cb65da5
Outlet <neonkamuy@gmail.com>
2021-03-30 17:57:05 +0400

    Various fixes

--- front/src/components/burger-modal/burger-button/index.tsx
+++ front/src/components/burger-modal/burger-button/index.tsx
@@ -1,15 +1,17 @@
 import React, { useCallback, useRef } from "react";
 import "../styles/button.scss";
+import "../styles/button-x.scss";
 import { IProps } from "./interfaces";
 
 export const BurgerButton: React.FC<IProps> = (props) => {
     const { isOpen, onClick } = props;
 
     return (
-        <div onClick={onClick} className={buttonClass + (isOpen ? "is-active" : "")}>
+        <div onClick={onClick} className={isOpen ? buttonXClass : buttonClass}>
             <span>toggle menu</span>
         </div>
     );
 }
 
-const buttonClass = "c-hamburger c-hamburger--x ";
+const buttonClass = "c-hamburger";
+const buttonXClass = "c-hamburger--x";
