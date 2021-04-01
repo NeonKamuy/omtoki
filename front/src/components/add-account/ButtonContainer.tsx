32415c8cdeb3cc6495de844f51cd902e818665a1
unknown <neonkamuy@gmail.com>
2021-04-01 21:44:40 +0400

    Update

--- front/src/components/add-account/ButtonContainer.tsx
+++ front/src/components/add-account/ButtonContainer.tsx
@@ -4,10 +4,10 @@ import { AccountModalButton } from ".";
 export const AccountButtonContainer = React.memo(() => {
   return (
     <div id="account-button-container">
-      <span className="slogan">
+      <div className="slogan">
         Краснополянская it-комунна. Мы решили не ехать в Долину, так как Поляна
         уже накрыта.
-      </span>
+      </div>
       <AccountModalButton />
       <span className="mention">
         Заявки проходят модерацию в течение 24 часов.
