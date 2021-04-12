4a153d29e40a27165f9407bcfbce14b24b950a9a
unknown <hfwalter73@gmail.com>
2021-04-13 01:01:54 +0400

    If modified since header

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as BodyParser from "body-parser";
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
-
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, { cors: true });
+  app.set('etag', false);
+  
   const httpAdapter = app.getHttpAdapter();
   httpAdapter.use(BodyParser.urlencoded({
     limit: "5mb",
